e50552e9fc1fc2ae02360cbf7151f184edce1c3c
WX_Nick <ninglianchengwork@126.com>
2022-03-18 20:18:46 +0800

    在Doc文件夹中增加两个文件

---
+++ V6-000/Doc/基本定时器TIM6使用.C
@@ -0,0 +1,23 @@
+  /* Compute the prescaler value to have TIM3 counter clock equal to 10 KHz */
+  uwPrescalerValue = (uint32_t) ((SystemCoreClock /2) / 10000) - 1;
+  
+  /* Set TIMx instance */
+  TimHandle.Instance = TIMx;
+   
+  /* Initialize TIM3 peripheral as follows:
+       + Period = 10000 - 1
+       + Prescaler = ((SystemCoreClock/2)/10000) - 1
+       + ClockDivision = 0
+       + Counter direction = Up
+  */
+  TimHandle.Init.Period = 10000 - 1;
+  TimHandle.Init.Prescaler = uwPrescalerValue;
+  TimHandle.Init.ClockDivision = 0;
+  TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
+  TimHandle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  
+  https://blog.csdn.net/weixin_43482414/article/details/103938834
+  
+  
+  
+  
